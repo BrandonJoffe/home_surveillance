00d8396fcc8a60711b6d6a0ff98c51cdfd91ccf6
Unknown <mduperre@cisco.com>
2017-03-15 12:19:51 -0400

    .

--- system/Camera.py
+++ system/Camera.py
@@ -82,7 +82,7 @@ class IPCamera(object):
 		self.captureEvent.set()
 		self.peopleDictLock = threading.Lock() # Used to block concurrent access to people dictionary
 		self.video = cv2.VideoCapture(camURL) # VideoCapture object used to capture frames from IP camera
-		logger.info("Capture properties:" + self.video.get(cv.CV_CAP_PROP_FRAME_COUNT)
+		logger.info("Capture properties:" + self.video.get(cv.CV_CAP_PROP_FRAME_COUNT))
 	 	self.url = camURL
 		if not self.video.isOpened():
 			self.video.open()		
