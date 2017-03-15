f4f04b83b8ea2992aa93c604628f91ed383c9330
Unknown <mduperre@cisco.com>
2017-03-15 11:26:50 -0400

    capture feed tool

---
+++ captureFeed.py
@@ -0,0 +1,29 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture('rtsp://198.48.221.102:554/user=admin_password=RgNNc3qH_channel=1_stream=0.sdp')
+
+
+# Define the codec and create VideoWriter object
+#fourcc = cv2.VideoWriter_fourcc(*'XVID')
+fourcc = cv2.cv.CV_FOURCC(*'XVID')
+out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if ret==True:
+        frame = cv2.flip(frame,0)
+
+        # write the flipped frame
+        out.write(frame)
+
+#        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+# Release everything if job is finished
+cap.release()
+out.release()
+cv2.destroyAllWindows()
