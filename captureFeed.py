303042de2088b85d3026544150b1f9b503e44ca3
Unknown <mduperre@cisco.com>
2017-03-16 11:00:30 -0400

    capture tool updated with args

--- captureFeed.py
+++ captureFeed.py
@@ -1,28 +1,60 @@
+#!/usr/bin/python
+#
+#
+# side tool to capture a feed with openCV and get an analysis of it
+# Mathieu Duperre
+# 2017
+#
+#
+
 import numpy as np
 import cv2
+import sys, getopt
 
-cap = cv2.VideoCapture('rtsp://198.48.221.102:554/user=admin_password=RgNNc3qH_channel=1_stream=0.sdp')
+def main(argv):
+    videofeed = ''
+    try:
+        opts, args = getopt.getopt(argv, "hf")
+    except getopt.GetoptError:
+        print
+        'captureFeed.py -f <video feed url>'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print
+            'captureFeed.py -f <video feed url>'
+            sys.exit()
+        elif opt in ("-f"):
+            videofeed = arg
 
-# Define the codec and create VideoWriter object
-# fourcc = cv2.VideoWriter_fourcc(*'XVID')
-fourcc = cv2.cv.CV_FOURCC(*'MJPG')
-out = cv2.VideoWriter('output.avi', fourcc, 20.0, (1280, 720))
+    print "---------OpenCV Video feed analyzer -------------\n\n\n"
+    print "Analyzing url: " + videofeed
+    cap = cv2.VideoCapture(videfeed)
 
-while (cap.isOpened()):
-    ret, frame = cap.read()
-    if ret == True:
-        frame = cv2.flip(frame, 0)
+    # Define the codec and create VideoWriter object
+    # fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    fourcc = cv2.cv.CV_FOURCC(*'MJPG')
+    out = cv2.VideoWriter('output.avi', fourcc, 20.0, (1280, 720))
 
-        # write the flipped frame
-        out.write(frame)
+    while (cap.isOpened()):
+        ret, frame = cap.read()
+        if ret == True:
+            frame = cv2.flip(frame, 0)
 
-        #        cv2.imshow('frame',frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+            # write the flipped frame
+            out.write(frame)
+
+            #        cv2.imshow('frame',frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        else:
             break
-    else:
-        break
 
-# Release everything if job is finished
-cap.release()
-out.release()
-cv2.destroyAllWindows()
+    # Release everything if job is finished
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
